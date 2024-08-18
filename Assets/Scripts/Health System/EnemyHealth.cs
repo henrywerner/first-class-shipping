3a7293684ca5333a72d4d75fa7f0d2f840d43a13
Andrew Sylvester <andrewrsylvester@gmail.com>
2024-08-18 18:08:00 -0500

    Enemy hit flash

--- Assets/Scripts/Health System/EnemyHealth.cs
+++ Assets/Scripts/Health System/EnemyHealth.cs
@@ -8,7 +8,9 @@ public class EnemyHealth : HealthSystem
     {
         Enemy enemy = GetComponent<Enemy>();
         if (enemy != null)
+        {
             EventManager.current.ShakeScreen(enemy.ScreenShakeDuration, enemy.ScreenShakeMagnitude);
+        }
 
         EventManager.current.EnemyKilled(); // Send enemy killed event
         this.gameObject.GetComponent<LootContainer>()?.DumpLoot(); // Drop held loot (if any)
