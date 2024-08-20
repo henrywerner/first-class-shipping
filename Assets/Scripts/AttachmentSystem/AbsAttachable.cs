594dde9767f4c88351c361b55bf2c1c4142f160e
Noah Sims <noahsims24@gmail.com>
2024-08-19 19:59:04 -0500

    Add pickup and damage indicators to guns

--- Assets/Scripts/AttachmentSystem/AbsAttachable.cs
+++ Assets/Scripts/AttachmentSystem/AbsAttachable.cs
@@ -11,8 +11,10 @@ public abstract class AbsAttachable : MonoBehaviour
     public AbsAttachable ParrentAttachment = null;
     [SerializeField] float detatchCooldown = 0.5f;
     [SerializeField] GameObject attachSFX;
-    private bool _hasDetached = false;
+    private bool _hasAttached = false;
     [SerializeField] GameObject attachVFX;
+    [SerializeField] GameObject _pickupIndicator;
+    private bool _isFlashing = false;
 
     public static event Action OnAttachmentsUpdate;
 
@@ -23,7 +25,7 @@ public abstract class AbsAttachable : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (_hasDetached)
+        if (ParrentAttachment == null)
         {
             Vector3 cameraBounds = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
             float boundsX = cameraBounds.x - (transform.lossyScale.x * 0.5f);
@@ -34,11 +36,16 @@ public abstract class AbsAttachable : MonoBehaviour
                 this.gameObject.SetActive(false);
             }
         }
+
+        if (!_hasAttached && !_isFlashing && _pickupIndicator != null)
+        {
+            StartCoroutine(FlashingPickup());
+        }
     }
 
     public void AttachTo(AbsAttachable argParentAttachment)
     {
-        _hasDetached = false;
+        _hasAttached = true;
         PickupDrifting drift = this.GetComponent<PickupDrifting>();
         if (drift != null)
         {
@@ -95,7 +102,6 @@ public abstract class AbsAttachable : MonoBehaviour
 
     virtual protected void Detach()
     {
-        _hasDetached = true;
         this.transform.parent = null;
         ParrentAttachment.NextAttachment = null;
         ParrentAttachment = null;
@@ -147,5 +153,40 @@ public abstract class AbsAttachable : MonoBehaviour
             return;
         }
 
+    }
+
+    private IEnumerator FlashingPickup()
+    {
+        _isFlashing = true;
+        _pickupIndicator.SetActive(true);
+        yield return StartCoroutine(LerpColor(0, 0.4f, 15));
+        yield return StartCoroutine(LerpColor(0.4f, 0, 15));
+        _pickupIndicator.SetActive(false);
+        // wait 30 frames
+        for (int i = 0; i < 30; i++)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        
+        _isFlashing = false;
+    }
+
+    private IEnumerator LerpColor(float alphaStart, float alphaEnd, int frames)
+    {
+        Color startColor = _pickupIndicator.GetComponent<SpriteRenderer>().material.color;
+        startColor.a = alphaStart;
+        _pickupIndicator.GetComponent<SpriteRenderer>().material.color = startColor;
+        Color colorGoal = startColor;
+        colorGoal.a = alphaEnd;
+
+        int framesElapsed = 0;
+
+        while (_pickupIndicator.GetComponent<SpriteRenderer>().material.color.a != alphaEnd && !_hasAttached)
+        {
+            Debug.Log("alpha value = " + _pickupIndicator.GetComponent<SpriteRenderer>().material.color.a);
+            _pickupIndicator.GetComponent<SpriteRenderer>().material.color = Color.Lerp(startColor, colorGoal, (float)framesElapsed / frames);
+            yield return new WaitForFixedUpdate();
+            framesElapsed++;
+        }
     }
 }
